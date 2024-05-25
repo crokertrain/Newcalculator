f2302c3451a7fce77ac0f1d43aa1b742d28016d7
Dave Angel <dave.angel@gmail.com>
2024-05-25 10:17:38 +0100

    All Files

---
+++ calc/calc.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+##############################################################################
+#                                                                            #
+# Script Name     : proj9b.py                                                #
+# Description     : This program shows an example CGI                        #
+# Author          : Neville Croker                                           #
+# Date            : 21 April 2022                                            #
+# Version         : 1.0.0                                                    #
+#----------------------------------------------------------------------------#
+#                                                                            #
+#                  PROPRIETARY AND CONFIDENTIAL INFORMATION                  #
+#                    AND INTELLECTUAL PROPERTY OF CROKER.                    #
+#                              (C) 2022 CROKER.                              #
+#                            ALL RIGHTS RESERVED.                            #
+#                                                                            #
+# NOTE        : DO NOT MAKE ANY CHANGES TO THIS PROGRAM.........             #
+#             : Please feel free to copy this program.                       #
+#                                                                            #
+#--------------------------- Amendment History ------------------------------#
+# Date        | Author             | Description                             #
+#----------------------------------------------------------------------------#
+# dd/mm/yyyy  |                    |                                         #
+##############################################################################
+
+# Import modules for CGI handling
+import cgi, cgitb
+
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
+
+# Get data from fields
+first_num = form.getvalue('first_number')
+last_num = form.getvalue('second_number')
+oper = form.getvalue('operator')
+
+if oper == "+":
+   answer = int(first_num) + int(last_num)
+elif oper == "-":
+   answer = int(first_num) - int(last_num)
+elif oper == "/":
+   answer = int(first_num) / int(last_num)
+elif oper == "*":
+   answer = int(first_num) * int(last_num)
+else:
+   answer = "Something is wrong"
+
+print("Content-type:text/html\r\n\r\n")
+print("<html>")
+print("<head>")
+
+print("<title>Calculator</title>")
+print("</head>")
+print("<body>")
+
+print("<h2>The result of the calculation %s %s %s</h2>" % (first_num, oper, last_num))
+
+print("<h2>Is %s</h2>" % answer)
+
+print("</body>")
+print("</html>")
+
